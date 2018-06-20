42a282451312fc786cf0c36f3c993ec88424e68d
Oleksii Khilkevych <oleksii.khilkevych@osi.ca.gov>
2018-06-20 16:44:09 -0700

    IRT fix.

--- src/test/java/gov/ca/cwds/rest/IntegratedResourceTestSuiteIT.java
+++ src/test/java/gov/ca/cwds/rest/IntegratedResourceTestSuiteIT.java
@@ -3,6 +3,7 @@ package gov.ca.cwds.rest;
 import gov.ca.cwds.rest.resources.ParticipantIntakeApiResourceIRT;
 import gov.ca.cwds.rest.resources.ScreeningIntakeResourceIRT;
 import gov.ca.cwds.rest.resources.auth.AuthorizationResourceIRT;
+import gov.ca.cwds.rest.resources.contact.ContactResourceIRT;
 import gov.ca.cwds.rest.resources.hoi.HoiCaseResourceAuthorizationIRT;
 import gov.ca.cwds.rest.resources.hoi.HoiCaseResourceIRT;
 import gov.ca.cwds.rest.resources.hoi.HoiReferralResourceIRT;
@@ -26,7 +27,8 @@ import org.junit.runners.Suite;
     InvolvementHistoryResourceIRT.class,
     ScreeningIntakeResourceIRT.class,
     ParticipantIntakeApiResourceIRT.class,
-    ScreeningRelationshipResourceIRT.class
+    ScreeningRelationshipResourceIRT.class,
+    ContactResourceIRT.class
 })
 public class IntegratedResourceTestSuiteIT {
 
