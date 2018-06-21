bb37a482829e95ce5783e0a9e6ac7ad6172cbf21
Shahid Saleemi <shahid.saleemi@osi.ca.gov>
2018-06-21 16:24:00 -0700

    Enable test data loading at server startup along with system codes.

--- src/test/java/gov/ca/cwds/rest/IntegratedResourceTestSuiteIT.java
+++ src/test/java/gov/ca/cwds/rest/IntegratedResourceTestSuiteIT.java
@@ -1,5 +1,9 @@
 package gov.ca.cwds.rest;
 
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
 import gov.ca.cwds.rest.resources.ParticipantIntakeApiResourceIRT;
 import gov.ca.cwds.rest.resources.ScreeningIntakeResourceIRT;
 import gov.ca.cwds.rest.resources.auth.AuthorizationResourceIRT;
@@ -11,30 +15,16 @@ import gov.ca.cwds.rest.resources.hoi.HoiUsingClientIdResourceIRT;
 import gov.ca.cwds.rest.resources.hoi.InvolvementHistoryResourceIRT;
 import gov.ca.cwds.rest.resources.relationship.ScreeningRelationshipResourceIRT;
 import gov.ca.cwds.test.support.BaseDropwizardApplication;
-import org.junit.ClassRule;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({
-    AuthorizationResourceIRT.class,
-    HoiCaseResourceAuthorizationIRT.class,
-    HoiCaseResourceIRT.class,
-    HoiReferralResourceIRT.class,
-    HoiScreeningResourceIRT.class,
-    HoiUsingClientIdResourceIRT.class,
-    InvolvementHistoryResourceIRT.class,
-    ScreeningIntakeResourceIRT.class,
-    ParticipantIntakeApiResourceIRT.class,
-    ScreeningRelationshipResourceIRT.class
-})
+@Suite.SuiteClasses({AuthorizationResourceIRT.class, HoiCaseResourceAuthorizationIRT.class,
+    HoiCaseResourceIRT.class, HoiReferralResourceIRT.class, HoiScreeningResourceIRT.class,
+    HoiUsingClientIdResourceIRT.class, InvolvementHistoryResourceIRT.class,
+    ScreeningIntakeResourceIRT.class, ParticipantIntakeApiResourceIRT.class,
+    ScreeningRelationshipResourceIRT.class})
 public class IntegratedResourceTestSuiteIT {
 
   @ClassRule
-  public static final BaseDropwizardApplication<ApiConfiguration> application =
-      new BaseDropwizardApplication<>(ApiApplication.class, "config/test-api.yml");
-
-  protected BaseDropwizardApplication<ApiConfiguration> getApplication() {
-    return application;
-  }
+  public static BaseDropwizardApplication<ApiConfiguration> application =
+      ApiApplicationTestSupport.getApplication();
 }
