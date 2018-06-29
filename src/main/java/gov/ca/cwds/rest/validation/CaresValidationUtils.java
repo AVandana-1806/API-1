f9f7a0cb2eff04abdef5d4a65f99f64920abbe31
David R. Smith <David.Smith@osi.ca.gov>
2018-06-29 12:19:00 -0700

    JUnit, Javadoc, SonarQube

--- src/main/java/gov/ca/cwds/rest/validation/CaresValidationUtils.java
+++ src/main/java/gov/ca/cwds/rest/validation/CaresValidationUtils.java
@@ -4,6 +4,10 @@ import gov.ca.cwds.rest.filters.RequestExecutionContext;
 
 public class CaresValidationUtils {
 
+  private CaresValidationUtils() {
+    // static methods only.
+  }
+
   public static boolean isBusinessValidationEnabled() {
     return RequestExecutionContext.instance().isXaTransaction()
         && "Y".equalsIgnoreCase(System.getProperty("DISABLE_BUSINESS_VALIDATION"));
