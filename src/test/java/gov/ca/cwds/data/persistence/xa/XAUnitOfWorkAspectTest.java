4e9f1f545ef4c44b911deef5c56a8166d6f1d8bb
David R. Smith <David.Smith@osi.ca.gov>
2018-06-28 19:38:41 -0700

    HOT-1956: XA transactions: Submit Screening to Referral

--- src/test/java/gov/ca/cwds/data/persistence/xa/XAUnitOfWorkAspectTest.java
+++ src/test/java/gov/ca/cwds/data/persistence/xa/XAUnitOfWorkAspectTest.java
@@ -15,6 +15,7 @@ import org.hibernate.CacheMode;
 import org.hibernate.FlushMode;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableMap;
@@ -30,10 +31,14 @@ public class XAUnitOfWorkAspectTest extends Doofenshmirtz<Addresses> {
   XAUnitOfWork xaUnitOfWork;
 
   @Override
+  @Before
   public void setup() throws Exception {
     super.setup();
-    sessionFactories = ImmutableMap.<String, SessionFactory>of("cms", sessionFactory);
+
+    sessionFactories =
+        ImmutableMap.<String, SessionFactory>of(Api.DATASOURCE_XA_CMS, sessionFactory);
     xaUnitOfWork = mock(XAUnitOfWork.class);
+
     target = new XAUnitOfWorkAspect(sessionFactories);
     target.setXaUnitOfWork(xaUnitOfWork);
 
@@ -41,7 +46,6 @@ public class XAUnitOfWorkAspectTest extends Doofenshmirtz<Addresses> {
     when(xaUnitOfWork.value()).thenReturn(values);
     when(xaUnitOfWork.cacheMode()).thenReturn(CacheMode.NORMAL);
     when(xaUnitOfWork.flushMode()).thenReturn(FlushMode.MANUAL);
-
     when(xaUnitOfWork.readOnly()).thenReturn(false);
     when(xaUnitOfWork.transactional()).thenReturn(true);
   }
@@ -126,6 +130,104 @@ public class XAUnitOfWorkAspectTest extends Doofenshmirtz<Addresses> {
     Map<String, SessionFactory> actual = target.getSessionFactories();
     Map<String, SessionFactory> expected = this.sessionFactories;
     assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void beforeStart_A$Method$XAUnitOfWork() throws Exception {
+    final Method method =
+        MethodUtils.getMatchingMethod(this.getClass(), "beforeStart_A1", new Class[0]);
+    XAUnitOfWork xaUnitOfWork = mock(XAUnitOfWork.class);
+    target.beforeStart(method, xaUnitOfWork);
+  }
+
+  @Test(expected = CaresXAException.class)
+  public void beforeStart_A$Method$XAUnitOfWork_T$CaresXAException() throws Exception {
+    final Method method =
+        MethodUtils.getMatchingMethod(this.getClass(), "beforeStart_A1", new Class[0]);
+    when(xaUnitOfWork.transactional()).thenThrow(CaresXAException.class);
+    target.beforeStart(method, xaUnitOfWork);
+  }
+
+  @Test
+  public void afterEnd_A$() throws Exception {
+    target.afterEnd();
+  }
+
+  @Test(expected = CaresXAException.class)
+  public void afterEnd_A$_T$CaresXAException() throws Exception {
+    sessionFactories = mock(ImmutableMap.class);
+    when(sessionFactories.isEmpty()).thenThrow(IllegalStateException.class);
+    target = new XAUnitOfWorkAspect(sessionFactories);
+    target.setXaUnitOfWork(xaUnitOfWork);
+
+    when(xaUnitOfWork.transactional()).thenThrow(CaresXAException.class);
+    target.afterEnd();
+  }
+
+  @Test
+  public void grabSession_A$String$SessionFactory() throws Exception {
+    String key = null;
+    Session actual = target.grabSession(key, sessionFactory);
+    Session expected = null;
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void hasTransactionalFlag_A$() throws Exception {
+    boolean actual = target.hasTransactionalFlag();
+    boolean expected = false;
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void beginXaTransaction_A$() throws Exception {
+    target.beginXaTransaction();
+  }
+
+  @Test(expected = CaresXAException.class)
+  public void beginXaTransaction_A$_T$CaresXAException() throws Exception {
+    when(xaUnitOfWork.transactional()).thenThrow(CaresXAException.class);
+    target.beginXaTransaction();
+  }
+
+  @Test
+  public void rollbackSessionTransaction_A$Session() throws Exception {
+    Session session = mock(Session.class);
+    target.rollbackSessionTransaction(session);
+  }
+
+  @Test
+  public void rollback_A$() throws Exception {
+    target.rollback();
+  }
+
+  @Test(expected = CaresXAException.class)
+  public void rollback_A$_T$CaresXAException() throws Exception {
+    when(xaUnitOfWork.transactional()).thenThrow(CaresXAException.class);
+    target.rollback();
+  }
+
+  @Test
+  public void commit_A$() throws Exception {
+    target.commit();
+  }
+
+  @Test(expected = CaresXAException.class)
+  public void commit_A$_T$CaresXAException() throws Exception {
+    when(xaUnitOfWork.transactional()).thenThrow(CaresXAException.class);
+    target.commit();
+  }
+
+  @Test
+  public void getXaUnitOfWork_A$() throws Exception {
+    XAUnitOfWork actual = target.getXaUnitOfWork();
+    assertThat(actual, is(notNullValue()));
+  }
+
+  @Test
+  public void setXaUnitOfWork_A$XAUnitOfWork() throws Exception {
+    XAUnitOfWork xaUnitOfWork = mock(XAUnitOfWork.class);
+    target.setXaUnitOfWork(xaUnitOfWork);
   }
 
 }
