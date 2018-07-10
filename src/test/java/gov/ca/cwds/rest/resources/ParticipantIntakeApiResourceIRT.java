a7cb5dacee6998c6c06b174835f2bee888b4df41
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-07-10 15:57:24 -0700

    sonar fixes

--- src/test/java/gov/ca/cwds/rest/resources/ParticipantIntakeApiResourceIRT.java
+++ src/test/java/gov/ca/cwds/rest/resources/ParticipantIntakeApiResourceIRT.java
@@ -66,7 +66,6 @@ public class ParticipantIntakeApiResourceIRT extends IntakeBaseTest {
   }
 
   @Test
-  @Ignore
   public void testPut() throws Exception {
     String request =
         fixture("fixtures/gov/ca/cwds/rest/resources/participant-intake-api-put-request.json");
