4b3e2cbb95414db859cb6a02b910685c8308802d
tom parker <tom.parker@osi.ca.gov>
2018-07-11 12:08:09 -0700

    HOT-1986 Fix dependency issues do to business rules, and change relationship ids to use participant ids

--- src/main/java/gov/ca/cwds/data/access/dto/ClientRelationshipDtoBuilder.java
+++ src/main/java/gov/ca/cwds/data/access/dto/ClientRelationshipDtoBuilder.java
@@ -1,39 +1,59 @@
 package gov.ca.cwds.data.access.dto;
 
 import gov.ca.cwds.cms.data.access.dto.ClientRelationshipDTO;
+import gov.ca.cwds.data.legacy.cms.entity.enums.YesNoUnknown;
 import gov.ca.cwds.data.persistence.cms.BaseClient;
 import gov.ca.cwds.data.persistence.cms.Client;
+import gov.ca.cwds.rest.api.domain.Participant;
 import gov.ca.cwds.rest.api.domain.ScreeningRelationship;
+import gov.ca.cwds.rest.services.ClientParticipants;
 
 public class ClientRelationshipDtoBuilder {
   private String id;
-  private BaseClient client;
-  private BaseClient relatedClient;
+  private String clientId;
+  private String relatedClientId;
   private short type;
+  private ClientParticipants participants;
+  private boolean absentParentIndicator;
+  private String sameHomeStatus;
 
-
-  public ClientRelationshipDtoBuilder() {
-  }
-
-  public ClientRelationshipDtoBuilder(ScreeningRelationship relationship) {
+  public ClientRelationshipDtoBuilder(ScreeningRelationship relationship, ClientParticipants participants) {
+    if (participants == null){
+      throw new NullPointerException("Client Participants can not be null");
+    }
     id = relationship.getId();
     type =(short)relationship.getRelationshipType();
-    client = createClient(relationship.getClientId());
-    relatedClient = createClient(relationship.getRelativeId());
+    clientId = relationship.getClientId();
+    relatedClientId = relationship.getRelativeId();
+    this.participants = participants;
+    this.absentParentIndicator = relationship.isAbsentParentIndicator();
+    this.sameHomeStatus = relationship.getSameHomeStatus();
   }
 
   public ClientRelationshipDTO build(){
     ClientRelationshipDTO dto = new ClientRelationshipDTO();
     dto.setIdentifier(id);
-    dto.setPrimaryClient(client);
-    dto.setSecondaryClient(relatedClient);
+    dto.setPrimaryClient(createClient(clientId));
+    dto.setSecondaryClient(createClient(relatedClientId));
+    dto.setAbsentParentIndicator(absentParentIndicator);
+    dto.setSameHomeStatus(YesNoUnknown.fromCode(sameHomeStatus));
     dto.setType(type);
     return dto;
   }
 
   private BaseClient createClient(String clientId){
     BaseClient baseClient = new Client();
-    baseClient.setId(clientId);
+    baseClient.setId(findLegacyId(clientId));
     return baseClient;
+  }
+
+  private String findLegacyId(String id){
+    for( Participant participant : participants.getParticipants()){
+      Long participantId = participant.getId();
+      if(participantId.toString().equals(id)){
+        return participant.getLegacyDescriptor().getId();
+      }
+    }
+    return null;
   }
 }
