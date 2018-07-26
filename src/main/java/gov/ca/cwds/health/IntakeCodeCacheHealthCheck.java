d7081833dbe724743a2aacfd6fad33b1de6b6882
Shahid Saleemi <shahid.saleemi@osi.ca.gov>
2018-07-26 13:56:21 -0700

    Health checks for lov and system code caches

--- src/main/java/gov/ca/cwds/health/IntakeCodeCacheHealthCheck.java
+++ src/main/java/gov/ca/cwds/health/IntakeCodeCacheHealthCheck.java
@@ -6,18 +6,25 @@ import gov.ca.cwds.rest.api.domain.IntakeCodeCache;
 
 public class IntakeCodeCacheHealthCheck extends HealthCheck {
 
+  private static final long MINIMUM_CACHE_SIZE = 413;
+  private static final long MINIMUM_LOVS = 394;
+
   public IntakeCodeCacheHealthCheck() {
     super();
   }
 
   @Override
   protected Result check() throws Exception {
+    long cacheSize = IntakeCodeCache.global().getCacheSize();
     int lovCount = IntakeCodeCache.global().getAll().size();
 
-    if (lovCount > 0) {
-      return Result.healthy();
+    String message = "[Expected minimum LOV code cache size " + MINIMUM_CACHE_SIZE + ", found "
+        + cacheSize + "], [Expected minimum LOVs " + MINIMUM_LOVS + ", found " + lovCount + "]";
+
+    if (cacheSize >= MINIMUM_CACHE_SIZE && lovCount >= MINIMUM_LOVS) {
+      return Result.healthy(message);
     } else {
-      return Result.unhealthy("Intake LOV code cache is empty.");
+      return Result.unhealthy(message);
     }
   }
 }
--- src/main/java/gov/ca/cwds/health/SystemCodeCacheHealthCheck.java
+++ src/main/java/gov/ca/cwds/health/SystemCodeCacheHealthCheck.java
@@ -6,18 +6,26 @@ import gov.ca.cwds.rest.api.domain.cms.SystemCodeCache;
 
 public class SystemCodeCacheHealthCheck extends HealthCheck {
 
+  private static final long MINIMUM_CACHE_SIZE = 4872;
+  private static final long MINIMUM_SYSTEM_CODES = 4674;
+
   public SystemCodeCacheHealthCheck() {
     super();
   }
 
   @Override
   protected Result check() throws Exception {
+    long cacheSize = SystemCodeCache.global().getCacheSize();
     int count = SystemCodeCache.global().getAllSystemCodes().size();
 
-    if (count > 0) {
-      return Result.healthy();
+    String message = "[Expected minimum system code cache size " + MINIMUM_CACHE_SIZE + ", found "
+        + cacheSize + "], [Expected minimum system codes " + MINIMUM_SYSTEM_CODES + ", found "
+        + count + "]";
+
+    if (cacheSize >= MINIMUM_CACHE_SIZE && count >= MINIMUM_SYSTEM_CODES) {
+      return Result.healthy(message);
     } else {
-      return Result.unhealthy("System code cache is empty");
+      return Result.unhealthy(message);
     }
   }
 }
