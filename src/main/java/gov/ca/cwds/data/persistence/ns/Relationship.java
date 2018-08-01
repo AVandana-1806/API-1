eaf9c699483034fe738288c076657417f7261261
OlegKorniichuk <oleg.korniichuk@osi.ca.gov>
2018-07-31 17:25:16 -0700

    Fix entity exceeded parameters list

--- src/main/java/gov/ca/cwds/data/persistence/ns/Relationship.java
+++ src/main/java/gov/ca/cwds/data/persistence/ns/Relationship.java
@@ -8,8 +8,16 @@ import java.io.Serializable;
 import java.util.Date;
 
 import java.util.Optional;
-import javax.persistence.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import org.hibernate.annotations.GenericGenerator;
 
 import gov.ca.cwds.data.persistence.PersistentObject;
