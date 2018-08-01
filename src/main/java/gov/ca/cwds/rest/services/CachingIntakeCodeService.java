3b0ca9ef0074b7134216c71d07704b8e6ad01008
OlegKorniichuk <oleg.korniichuk@osi.ca.gov>
2018-08-01 15:57:39 -0700

    Fix map iteration

--- src/main/java/gov/ca/cwds/rest/services/CachingIntakeCodeService.java
+++ src/main/java/gov/ca/cwds/rest/services/CachingIntakeCodeService.java
@@ -134,9 +134,9 @@ public class CachingIntakeCodeService extends IntakeLovService implements Intake
     List<IntakeLov> ret = new ArrayList<>();
     Map<CacheKey, Object> intakeLovs = intakeCodeCache.asMap();
 
-    for (CacheKey key : intakeLovs.keySet()) {
-      if (CacheKey.SYSTEM_CODE_ID_TYPE.equals(key.getType())) {
-        ret.add((IntakeLov) intakeLovs.get(key));
+    for (Map.Entry<CacheKey, Object> entry : intakeLovs.entrySet()) {
+      if (CacheKey.SYSTEM_CODE_ID_TYPE.equals(entry.getKey().getType())) {
+        ret.add((IntakeLov) entry.getValue());
       }
     }
     return ret;
