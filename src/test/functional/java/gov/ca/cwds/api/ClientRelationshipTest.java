41eb5d56ff2f96e35ce5a5a6124541c4ef6f8104
jim lebeau <James.LeBeau@osi.ca.gov>
2018-08-01 07:30:42 -0700

    functional test

--- src/test/functional/java/gov/ca/cwds/api/ClientRelationshipTest.java
+++ src/test/functional/java/gov/ca/cwds/api/ClientRelationshipTest.java
@@ -1,6 +1,10 @@
 package gov.ca.cwds.api;
 
 import static io.restassured.RestAssured.given;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import gov.ca.cwds.api.builder.FunctionalTestingBuilder;
@@ -22,4 +26,46 @@ public class ClientRelationshipTest extends FunctionalTest {
     given().queryParam("token", token).when().post(clientRelationshipPath).then().statusCode(500);
   }
 
+  @Test
+  public void shouldReturnKnownRelationshipsOfPerson() {
+    String clientId = "1234567ABC";
+    Map<String, Object> queryParams = new HashMap<String, Object>();
+    queryParams.put("clientIds", clientId);
+    queryParams.put(functionalTestingBuilder.TOKEN, token);
+    
+    functionalTestingBuilder.getRequest(clientRelationshipPath, queryParams)
+      .then()
+      .assertThat()
+      .body("relationship_to.related_person_first_name", Matchers.hasItems("Anna","babe","Nina","Lawrence"))
+      .and()
+      .statusCode(200);
+    
+  }
+  
+  @Test
+  public void shouldReturnEmptyWhenPersonHasNoKnownRelationships() {
+    String clientId = "1234567ABC";
+    Map<String, Object> queryParams = new HashMap<String, Object>();
+    queryParams.put("clientIds", clientId);
+    queryParams.put(functionalTestingBuilder.TOKEN, token);
+    
+    functionalTestingBuilder.getRequest(clientRelationshipPath, queryParams)
+      .then()
+      .body("isEmpty()", Matchers.is(true))
+      .and()
+      .statusCode(200);
+  }
+  
+  @Test
+  public void shouldReturnErrorWhenUserNotAughoriziedToAccessClient() {
+    String clientId = "1234567ABC";
+    Map<String, Object> queryParams = new HashMap<String, Object>();
+    queryParams.put("clientIds", clientId);
+    queryParams.put(functionalTestingBuilder.TOKEN, token);
+    
+    functionalTestingBuilder.getRequest(clientRelationshipPath, queryParams)
+      .then()
+      .statusCode(403);    
+  }
+    
 }
