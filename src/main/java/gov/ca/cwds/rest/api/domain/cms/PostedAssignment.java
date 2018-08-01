744fd38bf47ee61a72e605ea5ecb37d52ffce103
OlegKorniichuk <oleg.korniichuk@osi.ca.gov>
2018-08-01 14:32:33 -0700

    Remove duplicated equals/hashcode

--- src/main/java/gov/ca/cwds/rest/api/domain/cms/PostedAssignment.java
+++ src/main/java/gov/ca/cwds/rest/api/domain/cms/PostedAssignment.java
@@ -1,8 +1,6 @@
 package gov.ca.cwds.rest.api.domain.cms;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import gov.ca.cwds.rest.api.Response;
@@ -41,23 +39,4 @@ public class PostedAssignment extends Assignment {
     return id;
   }
 
-  /**
-   * {@inheritDoc}
-   *
-   * @see java.lang.Object#hashCode()
-   */
-  @Override
-  public int hashCode() {
-    return HashCodeBuilder.reflectionHashCode(this, false);
-  }
-
-  /**
-   * {@inheritDoc}
-   *
-   * @see java.lang.Object#equals(java.lang.Object)
-   */
-  @Override
-  public boolean equals(Object obj) {
-    return EqualsBuilder.reflectionEquals(this, obj, false);
-  }
 }
