c054dd9bba26847c2ae58384c076f47d74abc545
David R. Smith <David.Smith@osi.ca.gov>
2018-08-08 19:21:33 -0700

    Ferb NS LOV health check

---
+++ src/main/java/gov/ca/cwds/data/persistence/xa/CaresHibernateHackersKit.java
@@ -0,0 +1,105 @@
+package gov.ca.cwds.data.persistence.xa;
+
+import java.sql.Connection;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.jdbc.Work;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gov.ca.cwds.data.BaseDaoImpl;
+import gov.ca.cwds.health.resource.LovDbCheck;
+
+/**
+ * Because CARES cares and wants to alleviate your suffering. We feel your pain.
+ * 
+ * @author CWDS API Team
+ */
+public class CaresHibernateHackersKit {
+
+  protected static final Logger LOGGER = LoggerFactory.getLogger(LovDbCheck.class);
+
+  /**
+   * Steal a JDBC connection from a Hibernate session. Closing the session automatically returns the
+   * connection to the pool.
+   * 
+   * <p>
+   * Don't abuse this; we know where you live. And never mind that van parked across the street from
+   * your place. And do change those abhorrent curtains; they clash with your carpet.
+   * </p>
+   * 
+   * @param session session to steal from
+   * @return the session's connection
+   */
+  public static Connection stealConnection(final Session session) {
+    final CaresWorkConnectionStealer work = new CaresWorkConnectionStealer();
+    doWork(session, work);
+    return work.getConnection();
+  }
+
+  /**
+   * Clear a Hibernate session to avoid transaction errors. Hibernate {@code session.clear()} may
+   * fail without a transaction. Create a new transaction or join an existing one, as needed.
+   * 
+   * @param session active Hibernate session
+   */
+  public static void clearSession(final Session session) {
+    try {
+      grabTransaction(session);
+      session.clear(); // Hibernate "duplicate object" bug
+    } catch (Exception e) {
+      LOGGER.warn("'clear' without transaction", e);
+    }
+  }
+
+  /**
+   * "Work-around" (gentle euphemism for <strong>HACK</strong>) for annoying condition where a
+   * transaction should have started but did not.
+   * 
+   * <p>
+   * Get the current transaction from the current session or start a new transaction.
+   * </p>
+   * 
+   * @param dao DAO
+   * @return current, active transaction
+   */
+  public static Transaction grabTransaction(final BaseDaoImpl<?> dao) {
+    return grabTransaction(dao.grabSession());
+  }
+
+  /**
+   * @param session active Hibernation session
+   * @return current, active transaction
+   * @see #grabTransaction(BaseDaoImpl)
+   */
+  public static Transaction grabTransaction(final Session session) {
+    Transaction txn = null;
+    try {
+      txn = session.beginTransaction();
+    } catch (Exception e) { // NOSONAR
+      txn = session.getTransaction();
+    }
+    return txn;
+  }
+
+  /**
+   * Generic method to execute a Hibernate Work implementation (arbitrary JDBC through Hibernate).
+   * 
+   * @param session active Hibernate session
+   * @param work Hibernate Work instance
+   */
+  public static void doWork(final Session session, Work work) {
+    clearSession(session);
+    grabTransaction(session);
+    session.doWork(work);
+    clearSession(session);
+  }
+
+  public static Connection prepConnection(final Session session) {
+    final CaresWorkConnectionStealer work = new CaresWorkConnectionStealer();
+    doWork(session, work);
+    return work.getConnection();
+  }
+
+}
--- src/main/java/gov/ca/cwds/health/resource/LovDbCheck.java
+++ src/main/java/gov/ca/cwds/health/resource/LovDbCheck.java
@@ -1,7 +1,9 @@
 package gov.ca.cwds.health.resource;
 
 import java.math.BigInteger;
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -9,21 +11,34 @@ import java.util.TreeMap;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.inject.Inject;
 
 import gov.ca.cwds.inject.NsSessionFactory;
 
+/**
+ * Health check for Postgres list of value (LOV) tables and views.
+ * 
+ * @author CWDS API Team
+ */
 public class LovDbCheck implements Pingable {
 
-  private static Map<String, Integer> lovTableCounts = new TreeMap<>();
+  protected static final Logger LOGGER = LoggerFactory.getLogger(LovDbCheck.class);
+
+  private static final Map<String, Integer> lovTableCounts;
+
   static {
-    lovTableCounts.put("INTAKE_LOV_CATEGORIES", 23);
-    lovTableCounts.put("INTAKE_ONLY_LOV_CATEGORIES", 4);
-    lovTableCounts.put("INTAKE_LOV_CODES", 527);
-    lovTableCounts.put("INTAKE_ONLY_LOV_CODES", 13);
-    lovTableCounts.put("SYSTEM_CODES", 4274);
-    lovTableCounts.put("VW_INTAKE_LOV", 542);
+    final Map<String, Integer> tables = new TreeMap<>();
+    tables.put("INTAKE_LOV_CATEGORIES", 23);
+    tables.put("INTAKE_ONLY_LOV_CATEGORIES", 4);
+    tables.put("INTAKE_LOV_CODES", 527);
+    tables.put("INTAKE_ONLY_LOV_CODES", 13);
+    tables.put("SYSTEM_CODES", 4274);
+    tables.put("VW_INTAKE_LOV", 542);
+
+    lovTableCounts = Collections.unmodifiableMap(tables);
   }
 
   private SessionFactory sessionFactory;
@@ -37,20 +52,15 @@ public class LovDbCheck implements Pingable {
   @Override
   public boolean ping() {
     boolean ok = true;
-    Session session = null;
     messages = new ArrayList<>();
 
-    try {
-      session = sessionFactory.openSession();
+    try (final Session session = sessionFactory.openSession()) {
       for (Map.Entry<String, Integer> entry : lovTableCounts.entrySet()) {
-        String tableName = entry.getKey();
-        boolean tableCountOk = checkTableCount(session, tableName, entry.getValue());
+        boolean tableCountOk = checkTableCount(session, entry.getKey(), entry.getValue());
         ok = ok && tableCountOk;
       }
     } finally {
-      if (session != null) {
-        session.close();
-      }
+      // Session goes out of scope
     }
 
     return ok;
@@ -61,21 +71,24 @@ public class LovDbCheck implements Pingable {
     return messages.toString();
   }
 
-  private boolean checkTableCount(Session session, String tableName, int expectedCount) {
-    boolean ok = true;
+  protected boolean checkTableCount(Connection con, String tableName, int expectedCount) {
     final Query<?> query = session.createNativeQuery("SELECT count(*) FROM " + tableName);
-    int count = ((BigInteger) query.list().get(0)).intValue();
-
-    if (count < expectedCount) {
-      ok = false;
-    }
+    final int count = ((BigInteger) query.list().get(0)).intValue();
 
     addMessage("[Expected at least " + expectedCount + " " + tableName + ", found " + count + "]");
+    return !(count < expectedCount);
+  }
 
-    return ok;
+  private boolean checkTableCount(Session session, String tableName, int expectedCount) {
+    final Query<?> query = session.createNativeQuery("SELECT count(*) FROM " + tableName);
+    final int count = ((BigInteger) query.list().get(0)).intValue();
+
+    addMessage("[Expected at least " + expectedCount + " " + tableName + ", found " + count + "]");
+    return !(count < expectedCount);
   }
 
   private void addMessage(String message) {
     messages.add(message);
   }
+
 }
