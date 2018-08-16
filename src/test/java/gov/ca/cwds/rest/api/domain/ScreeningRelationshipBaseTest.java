5f3cd77078696a77e4f67f5d547696dc2d8ef781
Mykhailo <m.zemlyanoi@gmail.com>
2018-08-16 15:21:14 -0700

    Merge Test class

--- src/test/java/gov/ca/cwds/rest/api/domain/ScreeningRelationshipBaseTest.java
+++ src/test/java/gov/ca/cwds/rest/api/domain/ScreeningRelationshipBaseTest.java
@@ -2,21 +2,83 @@ package gov.ca.cwds.rest.api.domain;
 
 import static org.junit.Assert.*;
 
+import gov.ca.cwds.data.cms.TestSystemCodeCache;
+import io.dropwizard.validation.ConstraintViolations;
 import java.util.Date;
+import java.util.Set;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import org.junit.Before;
 import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * @author CWDS TPT-3 Team
- */
 public class ScreeningRelationshipBaseTest {
+  public static final int RELATIONSHIP_TYPE = 190;
+  private TestSystemCodeCache testSystemCodeCache = new TestSystemCodeCache();
 
-  public static final int RELATIONSHIP_TYPE = 211;
-  ScreeningRelationshipBase relationshipBase;
+  private ScreeningRelationship relationship;
+  private Validator validator;
+
+  @Before
+  public void setup(){
+//    private final Validator validator = BaseValidator.newValidator();
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    validator = factory.getValidator();
+
+
+    relationship = new ScreeningRelationship();
+    Date now = new Date();
+    relationship.setId("123");
+    relationship.setClientId("PersonLegacyId");
+    relationship.setRelativeId("RelationLegacydId");
+    relationship.setRelationshipType(RELATIONSHIP_TYPE);
+    relationship.setAbsentParentIndicator(true);
+    relationship.setSameHomeStatus("N");
+    relationship.setStartDate(now);
+    relationship.setEndDate(now);
+    relationship.setLegacyId("1233456789");
+  }
+
+  @Test
+  public void clientIdShouldErrorWhenNull(){
+    relationship.setClientId(null);
+    Set errors = validator.validate(relationship);
+    assertThat(ConstraintViolations.format(errors)).containsOnly("clientId may not be empty");
+  }
+
+  @Test
+  public void relativeIdShouldErrorWhenNull(){
+    relationship.setRelativeId(null);
+    Set errors = validator.validate(relationship);
+    assertThat(ConstraintViolations.format(errors)).containsOnly("relativeId may not be empty");
+  }
+
+  @Test
+  public void releationshipTypeShouldErrorWhenNotValidValue(){
+    relationship.setRelationshipType(1);
+    Set errors = validator.validate(relationship);
+    assertThat(ConstraintViolations.format(errors)).containsOnly("relationshipType must be a valid system code for category CLNTRELC");
+  }
+
+  @Test
+  public void sameHomeStatusShouldErrorWhenNull(){
+    relationship.setSameHomeStatus(null);
+    Set errors = validator.validate(relationship);
+    assertThat(ConstraintViolations.format(errors)).containsOnly("sameHomeStatus may not be empty");
+  }
+
+  @Test
+  public void sameHomeStatusShouldErrorWhenNotCorrectValue(){
+    relationship.setSameHomeStatus("Bad Value");
+    Set errors = validator.validate(relationship);
+    assertThat(ConstraintViolations.format(errors)).containsOnly("sameHomeStatus must be one of [Y, N, U]");
+  }
 
   @Test
   public void shouldHaveAllFieldsWhenCreatingWithSimpleConstructor() {
     Date now = new Date();
-    relationshipBase = new ScreeningRelationshipBase();
+    ScreeningRelationshipBase relationshipBase = new ScreeningRelationshipBase();
     relationshipBase.setClientId("PersonLegacyId");
     relationshipBase.setRelativeId("RelationLegacydId");
     relationshipBase.setRelationshipType(RELATIONSHIP_TYPE);
@@ -40,7 +102,7 @@ public class ScreeningRelationshipBaseTest {
   public void shouldBeEqual() {
     Date now = new Date();
 
-    relationshipBase = new ScreeningRelationshipBase();
+    ScreeningRelationshipBase relationshipBase = new ScreeningRelationshipBase();
     relationshipBase.setClientId("PersonLegacyId");
     relationshipBase.setRelativeId("RelationLegacydId");
     relationshipBase.setRelationshipType(RELATIONSHIP_TYPE);
@@ -60,15 +122,15 @@ public class ScreeningRelationshipBaseTest {
     relationshipEqual.setEndDate(now);
     relationshipEqual.setLegacyId("1233456789");
 
-    assertEquals(this.relationshipBase, relationshipEqual);
-    assertEquals(relationshipEqual, this.relationshipBase);
+    assertEquals(relationshipBase, relationshipEqual);
+    assertEquals(relationshipEqual, relationshipBase);
   }
 
   @Test
   public void shouldNotBeEqual() {
     Date now = new Date();
 
-    relationshipBase = new ScreeningRelationshipBase();
+    ScreeningRelationshipBase relationshipBase = new ScreeningRelationshipBase();
     relationshipBase.setClientId("PersonLegacyId");
     relationshipBase.setRelativeId("RelationLegacydId");
     relationshipBase.setRelationshipType(RELATIONSHIP_TYPE);
@@ -89,6 +151,6 @@ public class ScreeningRelationshipBaseTest {
     relationshipNotEqual.setEndDate(now);
     relationshipNotEqual.setLegacyId("1233456789");
 
-    assertNotEquals(this.relationshipBase, relationshipNotEqual);
+    assertNotEquals(relationshipBase, relationshipNotEqual);
   }
 }
