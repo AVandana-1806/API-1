7b90457ff5590fc3bd50e6067b9f2aeaa9c4e0bc
Shahid Saleemi <shahid.saleemi@osi.ca.gov>
2018-08-21 01:26:10 -0700

    Refactored intake lov cache

---
+++ src/main/java/gov/ca/cwds/rest/api/domain/IntakeLovType.java
@@ -0,0 +1,27 @@
+package gov.ca.cwds.rest.api.domain;
+
+/**
+ * @author CWDS API Team
+ *
+ */
+public enum IntakeLovType {
+
+  LANGUAGE("language"),
+
+  ADDRESS_COUNTY("address_county"),
+
+  ADDRESS_TYPE("address_type");
+
+  private final String value;
+
+  private IntakeLovType(String value) {
+    this.value = value;
+  }
+
+  /**
+   * @return the value
+   */
+  public String getValue() {
+    return value;
+  }
+}
