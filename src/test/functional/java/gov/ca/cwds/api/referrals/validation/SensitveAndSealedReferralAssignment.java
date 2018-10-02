7b6244c89a12fe6c11d9b191a00cc3225c6c0085
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2018-10-02 10:39:28 -0700

    Functional Test clean up and added R10975

--- src/test/functional/java/gov/ca/cwds/api/referrals/validation/SensitveAndSealedReferralAssignment.java
+++ src/test/functional/java/gov/ca/cwds/api/referrals/validation/SensitveAndSealedReferralAssignment.java
@@ -1,21 +1,21 @@
 package gov.ca.cwds.api.referrals.validation;
 
 import static org.hamcrest.Matchers.notNullValue;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+
 import org.junit.Before;
 import org.junit.Test;
+
 import gov.ca.cwds.api.FunctionalTest;
 import gov.ca.cwds.api.builder.HttpRequestHandler;
 import gov.ca.cwds.api.builder.ResourceEndPoint;
 import gov.ca.cwds.fixture.ParticipantResourceBuilder;
 import gov.ca.cwds.fixture.ScreeningToReferralResourceBuilder;
-import gov.ca.cwds.rest.api.domain.CrossReport;
 import gov.ca.cwds.rest.api.domain.Participant;
 import gov.ca.cwds.rest.api.domain.ScreeningToReferral;
-import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
 
 /**
  * @author CWDS API Team
@@ -35,7 +35,7 @@ import io.restassured.response.ResponseBody;
  */
 public class SensitveAndSealedReferralAssignment extends FunctionalTest {
   String referralPath;
-  private HttpRequestHandler functionalTestBuilder;
+  private HttpRequestHandler httpRequestHandler;
 
   /**
    * 
@@ -44,50 +44,37 @@ public class SensitveAndSealedReferralAssignment extends FunctionalTest {
   public void setup() {
     // default user login for Staff with Social Worker priv
     referralPath = getResourceUrlFor(ResourceEndPoint.REFERRALS.getResourcePath());
-    functionalTestBuilder = new HttpRequestHandler();
+    httpRequestHandler = new HttpRequestHandler();
   }
-  
+
   @Test
   public void shouldReturn201WhenPostingSensitiveReferral() {
     ScreeningToReferral referral = buildScreeningToReferral("S");
 
-    functionalTestBuilder.postRequest(referral, referralPath, token)
-      .then()
-      .statusCode(201)
-      .and()
-      .body("legacy_id", notNullValue());
+    httpRequestHandler.postRequest(referral, referralPath, token).then().statusCode(201).and()
+        .body("legacy_id", notNullValue());
   }
-  
+
   @Test
   public void shouldReturn201WhenPostingSealedReferral() {
     ScreeningToReferral referral = buildScreeningToReferral("R");
-    functionalTestBuilder.postRequest(referral, referralPath, token)
-      .then()
-      .statusCode(201)
-      .and()
-      .body("legacy_id", notNullValue());
-   
+    httpRequestHandler.postRequest(referral, referralPath, token).then().statusCode(201).and()
+        .body("legacy_id", notNullValue());
+
   }
-  
+
   protected ScreeningToReferral buildScreeningToReferral(String limitedAccessCode) {
-    Set<CrossReport> emptyCrossReports = new HashSet();
-    Participant victim = new ParticipantResourceBuilder()
-        .setLegacyDescriptor(null)
-        .createVictimParticipant();
-    Participant Perp = new ParticipantResourceBuilder()
-        .setGender("M")
-        .setLegacyDescriptor(null)
+    Participant victim =
+        new ParticipantResourceBuilder().setLegacyDescriptor(null).createVictimParticipant();
+    Participant Perp = new ParticipantResourceBuilder().setGender("M").setLegacyDescriptor(null)
         .createPerpParticipant();
     Participant reporter = new ParticipantResourceBuilder().setGender("M").setLegacyDescriptor(null)
         .createReporterParticipant();
     Set<Participant> participants = new HashSet<>(Arrays.asList(victim, Perp, reporter));
     ScreeningToReferral referral = new ScreeningToReferralResourceBuilder()
-        .setLimitedAccessCode(limitedAccessCode)
-        .setassigneeStaffId(userInfo.getStaffId())
-        .setIncidentCounty(userInfo.getIncidentCounty())
-        .setParticipants(participants)
-        .setCrossReports(emptyCrossReports)
-        .createScreeningToReferral();
+        .setLimitedAccessCode(limitedAccessCode).setassigneeStaffId(userInfo.getStaffId())
+        .setIncidentCounty(userInfo.getIncidentCounty()).setParticipants(participants)
+        .setCrossReports(new HashSet<>()).createScreeningToReferral();
     return referral;
   }
 }
