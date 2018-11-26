53a10b001e5cfcefcf235798782ae3297b9e70c7
David R. Smith <David.Smith@osi.ca.gov>
2018-11-26 12:22:28 -0800

    SNAP-775: real-time Snapshot client data

--- src/main/java/gov/ca/cwds/data/es/transform/LiveElasticWorkConnectionStealer.java
+++ src/main/java/gov/ca/cwds/data/es/transform/LiveElasticWorkConnectionStealer.java
@@ -63,7 +63,10 @@ public class LiveElasticWorkConnectionStealer implements Work {
    */
   public static void enableBatchSettings(Connection con) throws SQLException {
     final String dbProductName = con.getMetaData().getDatabaseProductName();
-    con.setSchema(LiveElasticWorkConnectionStealer.getDBSchemaName());
+    final String schema = LiveElasticWorkConnectionStealer.getDBSchemaName();
+    if (StringUtils.isNotBlank(schema)) {
+      con.setSchema(schema);
+    }
     con.setAutoCommit(false);
 
     if (StringUtils.containsIgnoreCase(dbProductName, "db2")) {
