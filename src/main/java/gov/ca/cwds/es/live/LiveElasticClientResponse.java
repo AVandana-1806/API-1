f0ed9dbc0370d678f6d350353ac4148099ec376f
David R. Smith <David.Smith@osi.ca.gov>
2018-11-27 15:24:12 -0800

    SNAP-775: real-time Snapshot client data

--- src/main/java/gov/ca/cwds/es/live/LiveElasticClientResponse.java
+++ src/main/java/gov/ca/cwds/es/live/LiveElasticClientResponse.java
@@ -1,6 +1,7 @@
 package gov.ca.cwds.es.live;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
@@ -8,6 +9,7 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import gov.ca.cwds.data.es.ElasticSearchPerson;
+import gov.ca.cwds.data.std.ApiMarker;
 import gov.ca.cwds.rest.api.Request;
 import gov.ca.cwds.rest.api.Response;
 import gov.ca.cwds.rest.api.domain.ReportingDomain;
@@ -25,16 +27,69 @@ public class LiveElasticClientResponse extends ReportingDomain implements Respon
 
   private static final long serialVersionUID = 1L;
 
+  public static class LiveElasticClientHitsOuter implements ApiMarker {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("hits")
+    private final LiveElasticClientHitsInner hits;
+
+    public LiveElasticClientHitsOuter(LiveElasticClientHitsInner hits) {
+      this.hits = hits;
+    }
+
+    public LiveElasticClientHitsInner getHits() {
+      return hits;
+    }
+
+  }
+
+  public static class LiveElasticClientHitsInner implements ApiMarker {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("hits")
+    private final List<LiveElasticClientPerson> hits;
+
+    /**
+     * Construct from ElasticSearchPerson hits.
+     * 
+     * @param hits ElasticSearchPerson objects
+     */
+    public LiveElasticClientHitsInner(List<LiveElasticClientPerson> hits) {
+      this.hits = hits;
+    }
+
+  }
+
+  public static class LiveElasticClientPerson extends ElasticSearchPerson {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("_source")
+    private final ElasticSearchPerson person;
+
+    public LiveElasticClientPerson(ElasticSearchPerson person) {
+      this.person = person;
+    }
+
+    public ElasticSearchPerson getPerson() {
+      return person;
+    }
+
+  }
+
   @JsonProperty("hits")
-  private final List<ElasticSearchPerson> hits;
+  private final LiveElasticClientHitsOuter hits;
 
   /**
-   * Preferred constructor. Build from UI identifier.
+   * Construct from ElasticSearchPerson hits.
    * 
    * @param hits ElasticSearchPerson objects
    */
   public LiveElasticClientResponse(List<ElasticSearchPerson> hits) {
-    this.hits = hits;
+    this.hits = new LiveElasticClientHitsOuter(new LiveElasticClientHitsInner(
+        hits.stream().map(LiveElasticClientPerson::new).collect(Collectors.toList())));
   }
 
   @Override
