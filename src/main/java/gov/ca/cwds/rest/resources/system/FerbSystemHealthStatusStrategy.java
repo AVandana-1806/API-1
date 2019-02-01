5b9b5770620d721d907f1b1340fd88210644149b
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2019-01-31 17:36:28 -0800

    FIT-427 - Update healthcheck API for Ferb-api - fix

--- src/main/java/gov/ca/cwds/rest/resources/system/FerbSystemHealthStatusStrategy.java
+++ src/main/java/gov/ca/cwds/rest/resources/system/FerbSystemHealthStatusStrategy.java
@@ -8,7 +8,7 @@ public class FerbSystemHealthStatusStrategy implements SystemHealthStatusStrateg
 
   @Override
   public boolean getSystemHealthStatus(Map<String, Result> healthChecks) {
-    return healthChecks.entrySet().stream().filter(e -> !e.equals(Api.HealthCheck.SWAGGER_STATUS))
+    return healthChecks.entrySet().stream().filter(e -> !e.getKey().equals(Api.HealthCheck.SWAGGER_STATUS))
         .allMatch(e -> e.getValue().isHealthy());
   }
 }
