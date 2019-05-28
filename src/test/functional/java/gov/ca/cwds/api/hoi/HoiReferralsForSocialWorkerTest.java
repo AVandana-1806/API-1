043d5e8063ac14af21d672717ec61ccf13f983ff
Oleksii Khilkevych <oleksii.khilkevych@osi.ca.gov>
2019-05-28 15:11:57 -0700

    object mapper test

--- src/test/functional/java/gov/ca/cwds/api/hoi/HoiReferralsForSocialWorkerTest.java
+++ src/test/functional/java/gov/ca/cwds/api/hoi/HoiReferralsForSocialWorkerTest.java
@@ -119,6 +119,7 @@ public class HoiReferralsForSocialWorkerTest extends FunctionalTest {
     Response response = httpRequestHandler.postRequest(referrals, referralsPath, token);
     ObjectMapper mapper = new ObjectMapper();
     mapper.registerModule(new JavaTimeModule());
+    mapper.registerModule(new JodaModule());
     ScreeningToReferral screeningToReferral =
         mapper.readValue(response.asString(), ScreeningToReferral.class);
 
