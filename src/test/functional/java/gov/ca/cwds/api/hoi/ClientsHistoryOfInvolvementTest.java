c96b5312dc2416b215a8e6b93acd154f2cee0857
Oleksii Khilkevych <oleksii.khilkevych@osi.ca.gov>
2019-05-28 15:25:18 -0700

    object mapper test

--- src/test/functional/java/gov/ca/cwds/api/hoi/ClientsHistoryOfInvolvementTest.java
+++ src/test/functional/java/gov/ca/cwds/api/hoi/ClientsHistoryOfInvolvementTest.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -119,6 +120,7 @@ public class ClientsHistoryOfInvolvementTest extends FunctionalTest {
     Response response = httpRequestHandler.postRequest(referrals, referralsPath, token);
     ObjectMapper mapper = new ObjectMapper();
     mapper.registerModule(new JodaModule());
+    mapper.registerModule(new JavaTimeModule());
     ScreeningToReferral screeningToReferral =
         mapper.readValue(response.asString(), ScreeningToReferral.class);
 
