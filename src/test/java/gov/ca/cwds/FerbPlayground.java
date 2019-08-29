6758d79c07b3bbae6eb7d851a47a3104a49a9bdd
David R. Smith <David.Smith@osi.ca.gov>
2019-08-29 11:11:48 -0700

    CMO-99: Dora heap memory

---
+++ src/test/java/gov/ca/cwds/FerbPlayground.java
@@ -0,0 +1,19 @@
+package gov.ca.cwds;
+
+import gov.ca.cwds.data.persistence.cms.Client;
+
+public class FerbPlayground {
+
+  public void findEntityTable() {
+    final Client client = new Client();
+  }
+
+  public static void main(String[] args) {
+    try {
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+}
