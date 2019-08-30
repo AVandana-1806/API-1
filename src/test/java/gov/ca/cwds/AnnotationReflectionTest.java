e972d182ae8c167007cfce6e2a601f8e59b14983
David R. Smith <David.Smith@osi.ca.gov>
2019-08-30 09:38:41 -0700

    CMO-99: Dora stability

--- src/test/java/gov/ca/cwds/AnnotationReflectionTest.java
+++ src/test/java/gov/ca/cwds/AnnotationReflectionTest.java
@@ -10,19 +10,37 @@ import java.lang.reflect.Method;
 
 import javax.persistence.Table;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gov.ca.cwds.data.legacy.cms.entity.LongText;
+import gov.ca.cwds.data.persistence.cms.Address;
+import gov.ca.cwds.data.persistence.cms.Allegation;
 import gov.ca.cwds.data.persistence.cms.Client;
+import gov.ca.cwds.data.persistence.cms.ClientAddress;
 
 public class AnnotationReflectionTest {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(AnnotationReflectionTest.class);
 
   public static void main(String[] args) throws Exception {
     check(Class2.class.getMethod("num", new Class[0]));
 
     findTableName(Client.class);
+    findTableName(Allegation.class);
+    findTableName(ClientAddress.class);
+    findTableName(LongText.class);
+    findTableName(gov.ca.cwds.data.persistence.cms.LongText.class);
+    findTableName(Address.class);
+    findTableName(gov.ca.cwds.data.legacy.cms.entity.Address.class);
+    findTableName(gov.ca.cwds.data.persistence.ns.Address.class);
   }
 
   public static String findTableName(Class<?> klazz) {
-    final Table table = Client.class.getDeclaredAnnotation(Table.class);
-    return table.name();
+    final Table table = klazz.getDeclaredAnnotation(Table.class);
+    final String ret = table != null ? table.name() : null;
+    LOGGER.info("table: class: {}, name: \"{}\"", klazz, ret);
+    return ret;
   }
 
   public static void check(Method invokedMethod) {
